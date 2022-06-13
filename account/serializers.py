87e5e746d92bc8ee093375457c6fd7883e3ca829
Mohamed Eldefrawy <mohamed.ahmed.aly.d@gmail.com>
2022-06-14 01:50:11 +0200

    finish Create tag api request

--- account/serializers.py
+++ account/serializers.py
@@ -1,6 +1,5 @@
-from rest_framework import serializers
-
 from account.models import User
+from rest_framework import serializers
 
 
 class DeveloperCreateSerializer(serializers.ModelSerializer):
@@ -11,7 +10,8 @@ class DeveloperCreateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ["email", "password", "confirm_password", "gender", "developer"]
+        fields = ["email", "password", "confirm_password",
+                  "gender", "developer", "birth_date"]
 
     def create(self, validated_data):
         email = validated_data["email"]
@@ -19,11 +19,12 @@ class DeveloperCreateSerializer(serializers.ModelSerializer):
         confirm_password = validated_data["confirm_password"]
         gender = validated_data["gender"]
         developer = validated_data["developer"]
+        birth_date = validated_data["birth_date"]
         if email and User.objects.filter(email=email).exists():
             raise serializers.ValidationError({"email": "Email addresses must be unique."})
         if password != confirm_password:
             raise serializers.ValidationError({"password": "The two passwords differ."})
-        user = User(email=email, gender=gender, developer=developer)
+        user = User(email=email, gender=gender, birth_date=birth_date, developer=developer)
         user.set_password(password)
         user.save()
         return user
