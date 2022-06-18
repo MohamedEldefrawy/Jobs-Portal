54cd8e5d14ba6593ea5ec5102efa6b34dcd67b74
Mohamed Eldefrawy <mohamed.ahmed.aly.d@gmail.com>
2022-06-18 02:35:25 +0200

    fix senhd notifications issue

--- notification/api/v1/views.py
+++ notification/api/v1/views.py
@@ -10,3 +10,14 @@ def get(request, user_id):
     notifications = Notification.objects.filter(users_id=user_id).all()
     serialize = NotificationSerialize(notifications, many=True)
     return Response({'success': True, 'notifications': serialize.data}, status=status.HTTP_200_OK)
+
+
+@decorators.api_view(["PUT"])
+def update(request, notification_id):
+    selected_notification = Notification.objects.filter(id=notification_id).first()
+    if selected_notification.status == "true":
+        selected_notification.status = "false"
+    else:
+        selected_notification.status = "true"
+    selected_notification.save()
+    return Response({'success': True, 'message': "Status has been update successfully"})
